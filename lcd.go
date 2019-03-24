4c90e9ed6f4cbd9158845f9c923067a2d73fdf7d
Ben McClelland <ben.mcclelland@versity.com>
2019-03-24 10:42:15 -0700

    lcd: add SetText2 and ScrollText

--- lcd.go
+++ lcd.go
@@ -1,6 +1,8 @@
 package gogrove
 
 import (
+	"context"
+	"fmt"
 	"sync"
 	"time"
 
@@ -85,7 +87,8 @@ func (l *LCD) ClearText() error {
 	return nil
 }
 
-// SetText clears display text, and sets text.
+// SetText clears display text, and sets text, accepts newlines and
+// auto wraps long line.
 func (l *LCD) SetText(str string) error {
 	l.Lock()
 	defer l.Unlock()
@@ -96,23 +99,17 @@ func (l *LCD) SetText(str string) error {
 		return err
 	}
 
-	time.Sleep(50 * time.Millisecond)
-
 	// display on, no cursor
 	err = l.b.Tx(displayTextAddr, []byte{textCmd, 0x08 | 0x04}, nil)
 	if err != nil {
 		return err
 	}
 
-	time.Sleep(50 * time.Millisecond)
-
 	// 2 lines
 	err = l.b.Tx(displayTextAddr, []byte{textCmd, 0x28}, nil)
 	if err != nil {
 		return err
 	}
-
-	time.Sleep(50 * time.Millisecond)
 
 	count := 0
 	row := 0
@@ -139,4 +136,130 @@ func (l *LCD) SetText(str string) error {
 	}
 
 	return nil
+}
+
+// SetText2 clears display text, and sets text, accepts 2 lines
+// truncating after 16 chars
+func (l *LCD) SetText2(line1, line2 string) error {
+	l.Lock()
+	defer l.Unlock()
+
+	// clear
+	err := l.b.Tx(displayTextAddr, []byte{textCmd, 0x01}, nil)
+	if err != nil {
+		return err
+	}
+	// display on, no cursor
+	err = l.b.Tx(displayTextAddr, []byte{textCmd, 0x08 | 0x04}, nil)
+	if err != nil {
+		return err
+	}
+	// 2 lines
+	err = l.b.Tx(displayTextAddr, []byte{textCmd, 0x28}, nil)
+	if err != nil {
+		return err
+	}
+
+	err = l.displayLine(line1)
+	if err != nil {
+		return err
+	}
+
+	err = l.b.Tx(displayTextAddr, []byte{textCmd, 0xc0}, nil)
+	if err != nil {
+		return err
+	}
+
+	err = l.displayLine(line2)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (l *LCD) displayLine(str string) error {
+	count := 0
+	for _, c := range str {
+		if c == '\n' || count == 16 {
+			break
+		}
+		count++
+		err := l.b.Tx(displayTextAddr, []byte{0x40, uint8(c)}, nil)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ScrollText clears display text, and sets text, accepts 2 lines
+// scrolling both lines if longer than 16.  Pass a context to cancel to
+// terminate function, otherwise scolls forever.
+func (l *LCD) ScrollText(ctx context.Context, line1, line2 string) error {
+	l.Lock()
+	defer l.Unlock()
+
+	i := 0
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+
+			// clear
+			err := l.b.Tx(displayTextAddr, []byte{textCmd, 0x01}, nil)
+			if err != nil {
+				return err
+			}
+
+			// display on, no cursor
+			err = l.b.Tx(displayTextAddr, []byte{textCmd, 0x08 | 0x04}, nil)
+			if err != nil {
+				return err
+			}
+			// 2 lines
+			err = l.b.Tx(displayTextAddr, []byte{textCmd, 0x28}, nil)
+			if err != nil {
+				return err
+			}
+
+			stbegin := 0
+			frame := 16
+			if len(line1) > 16 {
+				stbegin = i % len(line1)
+			}
+			if len(line1)-stbegin < 16 {
+				frame = len(line1) - stbegin
+			}
+			fmt.Println(line1[stbegin:][:frame])
+			err = l.displayLine(line1[stbegin:][:frame])
+			if err != nil {
+				return err
+			}
+
+			err = l.b.Tx(displayTextAddr, []byte{textCmd, 0xc0}, nil)
+			if err != nil {
+				return err
+			}
+
+			stbegin = 0
+			frame = 16
+			if len(line2) > 16 {
+				stbegin = i % len(line2)
+			}
+			if len(line2)-stbegin < 16 {
+				frame = len(line2) - stbegin
+			}
+			fmt.Println(line2[stbegin:][:frame])
+			err = l.displayLine(line2[stbegin:][:frame])
+			if err != nil {
+				return err
+			}
+			fmt.Println()
+
+			time.Sleep(500 * time.Millisecond)
+			i++
+		}
+	}
 }
